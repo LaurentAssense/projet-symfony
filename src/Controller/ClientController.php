2654c43abd18ad5e62d70485ae88a2f0678d27c8
LaurentAssense <laurentassense.pro@gmail.com>
2025-12-04 14:11:25 +0100

    exo1 fin tp1

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -4,11 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+
 
 class ClientController extends AbstractController {
     
-    #[Route('/client/prenom/{prenom}', name: 'app_client_info')]
     function info(string $prenom): Response
     {
         if (!preg_match('/^[a-zA-Z-]+$/', $prenom)) {
