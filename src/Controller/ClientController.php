cac55f4171173672687ed8a4e52832446773dc80
LaurentAssense <laurentassense.pro@gmail.com>
2025-12-04 12:36:40 +0100

    exo1 fin tp

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -8,9 +8,16 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ClientController extends AbstractController {
     
-    #[Route('/client/prenom/{prenom}', 'app_client_info')]
+    #[Route('/client/prenom/{prenom}', name: 'app_client_info')]
     function info(string $prenom): Response
     {
+        if (!preg_match('/^[a-zA-Z-]+$/', $prenom)) {
+            return new Response(
+                'Le prénom ne doit contenir que des lettres et des tirets.',
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
         return new Response(
             "Le prenom du client est : $prenom"
         );
